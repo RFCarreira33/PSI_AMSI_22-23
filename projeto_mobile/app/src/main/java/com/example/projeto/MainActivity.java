bf7e46db45cf137c8187b1525d50fa97a4f2bf19
João Jesus <2211874@my.ipleiria.pt>
2022-12-18 20:24:32 +0000

    Adquirir a latitude e longitude do telefone

--- projeto_mobile/app/src/main/java/com/example/projeto/MainActivity.java
+++ projeto_mobile/app/src/main/java/com/example/projeto/MainActivity.java
@@ -3,21 +3,45 @@ package com.example.projeto;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+<<<<<<< Updated upstream
+=======
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentContainerView;
+>>>>>>> Stashed changes
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import Utils.Public;
 
 public class MainActivity extends AppCompatActivity {
 
     ActionBar actionBar = null;
+    TextView latitudes, longitudes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +53,29 @@ public class MainActivity extends AppCompatActivity {
         actionBar.setDisplayUseLogoEnabled(true);
         actionBar.setLogo(R.drawable.logo);
 
-    }
+        latitudes = findViewById(R.id.latitude);
+        longitudes = findViewById(R.id.longitude);
 
+        try {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            } else {
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+
+                longitudes.setText(String.valueOf(longitude));
+                latitudes.setText(String.valueOf(latitude));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,4 +119,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, QRReader.class);
         startActivity(intent);
     }
+    
 }
